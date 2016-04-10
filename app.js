dc92d4ffbf30ac4ebe8e0702b2b3f3f24995b10e
UnderSampled <undersampled@live.com>
2016-04-10 06:25:43 -0400

    Pass all messages except server messages

--- app.js
+++ app.js
@@ -15,20 +15,17 @@ wss.on('connection', function connection (ws) {
   ws.on('message', function incoming (msg) {
     console.log('received: %s', msg)
     var cmd = msg.split(' ')
-    switch (cmd[0]) {
-      case 'id':
-        var id = cmd[1]
-        user = users.find(function (user) { return user.id === id })
-        if (!user) {
-          user = {id: id, level: 0}
-          users.push(user)
-        }
-        break
-      case 'key':
-        for (var member of room) {
-          if (member !== ws) { member.send(['key', cmd[1]].join(' ')) }
-        }
-        break
+    if (cmd[0] === 'id') {
+      var id = cmd[1]
+      user = users.find(function (user) { return user.id === id })
+      if (!user) {
+        user = {id: id, level: 0}
+        users.push(user)
+      }
+    } else {
+      for (var member of room) {
+        if (member !== ws) { member.send(msg) }
+      }
     }
   })
 
